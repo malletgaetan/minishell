87d7fd9ad2e5198cf133b43f316bda922240654c
malletgaetan <mallet.gaetan@hotmail.fr>
2023-07-31 08:46:54 +0200

    fix: make 'ls | < text.txt cat' a valid command

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -12,22 +12,25 @@
 
 #include "minishell.h"
 
-int	set_next_token(char **line, t_token **t, t_token **bt, int lt)
+int	set_next_token(char **line, t_token **t)
 {
 	int	err;
 
 	if (ft_isoperator(**line))
-	{
-		if (lt != WORD)
-			err = set_bad_token(line, bt);
-		else
-			err = set_operator_token(line, t);
-	}
+		err = set_operator_token(line, t);
 	else
-	{
 		err = set_word_token(line, t);
-	}
 	return (err);
+}
+
+int	bad_syntax(int last_type, int new_type)
+{
+	if ((last_type == PIPE || last_type == -1) && new_type == PIPE)
+		return (1);
+	if ((last_type != PIPE && last_type != WORD) && \
+		(new_type != PIPE && new_type != WORD))
+		return (1);
+	return (0);
 }
 
 int	lex(char *line, t_token **token, t_token **bad_token)
@@ -36,18 +39,21 @@ int	lex(char *line, t_token **token, t_token **bad_token)
 	int		err;
 
 	g_ms.nb_cmds = 1;
-	ft_skipspaces(&line);
-	if (*line == '|')
-		return (set_bad_token(&line, bad_token));
-	last_type = WORD;
+	last_type = -1;
 	while (*line)
 	{
 		ft_skipspaces(&line);
 		if (*line == '\0')
 			break ;
-		err = set_next_token(&line, token, bad_token, last_type);
+		err = set_next_token(&line, token);
 		if (err != 0)
 			return (err);
+		if (bad_syntax(last_type, (*token)->type))
+		{
+			(*token)->type = BAD;
+			(*bad_token) = (*token);
+			return (LEXER_SYNTAX_ERROR);
+		}
 		last_type = (*token)->type;
 		token = &((*token)->next);
 	}
